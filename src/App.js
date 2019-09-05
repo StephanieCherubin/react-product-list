f95f9f6ea104b2923ba8f2985e943d70ef495379
Stephanie Cherubin <seraphin.cherubin@gmail.com>
2019-09-05 13:50:10 -0700

    create constructor for class

--- src/App.js
+++ src/App.js
@@ -3,18 +3,51 @@ import inventory, { categories } from './inventory'
 import './App.css';
 
 class App extends Component {
-  
+  constructor(props){
+    super(props)
+
+    this.state ={
+      category: 'All',
+    }
+  }
+
   render() {
+    const cats = categories.map((item, index) => {
+      return (
+        <button
+          key={(`category-${index}`)}
+          onClick={(e) => {
+            this.setState({ category: item});
+          }}
+        >{item}</button>
+      )
+    })
+
+    const products = inventory.filter((item) => {
+      return item.category === "Toys"
+    }).map(({ name, id, category, description, price }, index ) => {
+      return (
+        <div key={`product=${id}`}>
+          <h1>{name}</h1>
+          <p>{description}</p>
+          <p>{price}</p>
+          <p>{category}</p>
+        </div>
+      )
+    })
+
     return (
       <div className="App">
-        <h1>Show products here</h1>
+        <p>{this.state.category}</p>
+
+
 
         <ul>
-          {/* List product categories here */}
+          {cats}
         </ul>
 
         <ul>
-          {/* Products listed here */}
+          {products}
         </ul>
 
       </div>
